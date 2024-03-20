8beb5b4524afcade329e81cd799d311892df3c00
LAMezaCaballero <137646190+LAMezaCaballero@users.noreply.github.com>
2024-03-19 21:34:56 -0600

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, routing
 
 from fastapi.responses import JSONResponse
-from routers.my_modules import router1,router2,router3,router4,router5
+from routers.my_modules import router1,router2,router3,router4,router5,router6
 
 
 app=FastAPI()
@@ -15,4 +15,4 @@ app.include_router(router2)
 app.include_router(router3)
 app.include_router(router4)
 app.include_router(router5)
-
+app.include_router(router6)
